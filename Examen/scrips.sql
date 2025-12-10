717739e264b41b713e610db6f1e0b92935ad5c54
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-12-10 07:47:15 -0500

    feat: :fire: added TIMESTAMPDIFF

--- Examen/scrips.sql
+++ Examen/scrips.sql
@@ -1,36 +1,41 @@
 -- ================================= 35
 -- Scripts del Examen 10-12-2025 - MySql2
 -- =================================
-
+use pizzeria_piccolo_db;
 
 -- Consulta de entregas realizadas por cada repartidor
 -- Mostrar el nombre del repartidor, cantidad de entregas realizadas (estado='entregado'), y total acumulado de pedidos entregados.
-SELECT 
+SELECT
     d.id AS domiciliario_id,
     per.nombre AS repartidor,
-    COUNT(dom.id) AS total_entregas,
-FROM Domiciliarios d
-JOIN Personas per ON d.persona_id = per.id
-JOIN Domicilios dom ON d.id = dom.domiciliario_id
-JOIN Pedidos p ON d.id = p.id
-WHERE p.estado = 'entregado'
-GROUP BY d.id, per.nombre
+    COUNT(dom.id) AS total_entregas
+FROM
+    Domiciliarios d
+    JOIN Personas per ON d.persona_id = per.id
+    JOIN Domicilios dom ON d.id = dom.domiciliario_id
+    JOIN Pedidos p ON d.id = p.id
+WHERE
+    p.estado = 'entregado'
+GROUP BY
+    d.id,
+    per.nombre
 ORDER BY total_entregas DESC;
 
 -- Consulta de pedidos demorados
 -- Mostrar los pedidos cuya entrega tomó más de 40 minutos entre hora_salida y hora_entrega
 --  (Usa TIMESTAMPDIFF(MINUTE, hora_salida, hora_entrega) > 40).
 
-SELECT 
-    *, 
-    (TIMESTAMPDIFF(MINUTE, hora, hora_entrega)>40) 
-FROM
+SELECT *, (
+        TIMESTAMPDIFF (
+            MINUTE, d.hora_salida, d.hora_entrega
+        ) > 40
+    )
+FROM Pedidos p
+    JOIN Domicilios d;
 
 -- Consulta de repartidores activos sin entregas
 -- Mostrar los repartidores con estado 'activo' que no tienen domicilios asignados (usa LEFT JOIN y WHERE domicilio.id_domicilio IS NULL).
 SELECT
-
-
 
 -- Vista resumen de desempeño
 -- Crear una vista vista_desempeno_repartidor que muestre:
